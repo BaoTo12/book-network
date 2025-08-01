2860d685c46dec1bb08b05b438275763ddbb5ea1
ChiBao <baotochi87@gmail.com>
2025-08-01 09:06:09 +0700

    feat(profile): add create user profile method in user profile service

---
+++ profile-service/src/main/java/com/devteria/profile/service/UserProfileService.java
@@ -0,0 +1,28 @@
+package com.devteria.profile.service;
+
+
+import com.devteria.profile.dto.request.UserProfileCreationRequest;
+import com.devteria.profile.dto.response.UserProfileResponse;
+import com.devteria.profile.entity.UserProfile;
+import com.devteria.profile.mapper.UserProfileMapper;
+import com.devteria.profile.repo.UserProfileRepository;
+import jakarta.transaction.Transactional;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+@Transactional
+public class UserProfileService {
+    UserProfileRepository userProfileRepository;
+    UserProfileMapper mapper;
+
+    UserProfileResponse createUserProfile(UserProfileCreationRequest request) {
+        UserProfile userProfile = mapper.toUserProfile(request);
+        userProfile = userProfileRepository.save(userProfile);
+        return mapper.toUserProfileResponse(userProfile);
+    }
+}
