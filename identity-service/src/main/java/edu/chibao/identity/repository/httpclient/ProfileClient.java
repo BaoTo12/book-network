5ffbff40cdebb04c9784928c95f5ef4b25003607
ChiBao <baotochi87@gmail.com>
2025-08-01 12:08:26 +0700

    feat(identity): add ProfileClient open feign to communication with profile service

--- identity-service/src/main/java/edu/chibao/identity/repository/httpclient/ProfileClient.java
+++ identity-service/src/main/java/edu/chibao/identity/repository/httpclient/ProfileClient.java
@@ -1,4 +1,16 @@
 package edu.chibao.identity.repository.httpclient;
 
-public class ProfileClient {
+import edu.chibao.identity.dto.request.UserProfileCreationRequest;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "profile-service", url = "http://localhost:8081/profile")
+public interface ProfileClient {
+    // when call to this function open feign will make a call with post method, requestUrl = url + /users
+    // and return Content-type: Application/json
+    @PostMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
+    Object createUserProfile(@RequestBody UserProfileCreationRequest request);
+
 }
