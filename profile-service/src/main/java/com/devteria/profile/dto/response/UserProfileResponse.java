ac17ef7ad9bafa16d7daa9b04164aef10e9c7375
ChiBao <baotochi87@gmail.com>
2025-08-01 08:59:59 +0700

    feat(profile): add UserProfileResponse

---
+++ profile-service/src/main/java/com/devteria/profile/dto/response/UserProfileResponse.java
@@ -0,0 +1,21 @@
+package com.devteria.profile.dto.response;
+
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@NoArgsConstructor
+public class UserProfileResponse {
+    String firstName;
+    String lassName;
+    LocalDate dob;
+    String city;
+}
