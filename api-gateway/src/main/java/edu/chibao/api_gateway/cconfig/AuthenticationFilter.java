65fae2ad72b568262a2e9b9224d37b0e295ac09a
ChiBao <baotochi87@gmail.com>
2025-08-01 21:11:20 +0700

    feat(api-gateway): add authentication filter for requests to api gateway

--- api-gateway/src/main/java/edu/chibao/api_gateway/cconfig/AuthenticationFilter.java
+++ api-gateway/src/main/java/edu/chibao/api_gateway/cconfig/AuthenticationFilter.java
@@ -1,4 +1,42 @@
 package edu.chibao.api_gateway.cconfig;
 
-public class AuthenticationFilter {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import java.util.List;
+
+
+@Component
+@Slf4j
+public class AuthenticationFilter implements GlobalFilter, Ordered {
+    @Override
+
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("Enter Global Filter for Authentication ...");
+        // Get Token
+        List<String> authHeaders = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION);
+        if(CollectionUtils.isEmpty(authHeaders))
+            return unauthenticated(exchange.getResponse());
+        // Verify token
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+
+    Mono<Void> unauthenticated(ServerHttpResponse response){
+        String msg = "You are not authenticated";
+        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+        return response.writeWith(Mono.just(response.bufferFactory().wrap(msg.getBytes())));
+    }
 }
