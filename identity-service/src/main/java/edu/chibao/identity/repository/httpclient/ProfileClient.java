cd0b9f204162405e676deadfc495364c42245d0f
ChiBao <baotochi87@gmail.com>
2025-08-01 14:32:13 +0700

    ref: create service name of profile service in application.yml

--- identity-service/src/main/java/edu/chibao/identity/repository/httpclient/ProfileClient.java
+++ identity-service/src/main/java/edu/chibao/identity/repository/httpclient/ProfileClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "profile-service", url = "http://localhost:8081/profile")
+@FeignClient(name = "profile-service", url = "${app.service.profile}")
 public interface ProfileClient {
     // when call to this function open feign will make a call with post method, requestUrl = url + /users
     // and return Content-type: Application/json
