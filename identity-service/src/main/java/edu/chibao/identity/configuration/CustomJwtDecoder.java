8434ffa688893f2224f6aaaae49a8d4043b0a7c0
ChiBao <baotochi87@gmail.com>
2025-08-02 08:13:45 +0700

    ref(identity): remove authentication login in CustomJwtDecoder

--- identity-service/src/main/java/edu/chibao/identity/configuration/CustomJwtDecoder.java
+++ identity-service/src/main/java/edu/chibao/identity/configuration/CustomJwtDecoder.java
@@ -1,52 +1,36 @@
 package edu.chibao.identity.configuration;
 
-import com.nimbusds.jose.JOSEException;
-import edu.chibao.identity.dto.request.IntrospectRequest;
+import com.nimbusds.jwt.SignedJWT;
 import edu.chibao.identity.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.spec.SecretKeySpec;
 import java.text.ParseException;
-import java.util.Objects;
+import java.time.Instant;
 
 @Component
 public class CustomJwtDecoder implements JwtDecoder {
     @Value("${jwt.signerKey}")
     private String signerKey;
 
-    private final AuthenticationService authenticationService;
-
-    private NimbusJwtDecoder nimbusJwtDecoder = null;
-
-    public CustomJwtDecoder(AuthenticationService authenticationService) {
-        this.authenticationService = authenticationService;
-    }
-
     @Override
     public Jwt decode(String token) throws JwtException {
-
         try {
-            var response = authenticationService.introspect(
-                    IntrospectRequest.builder().token(token).build());
-
-            if (!response.isValid()) throw new JwtException("Token invalid");
-        } catch (JOSEException | ParseException e) {
-            throw new JwtException(e.getMessage());
+            SignedJWT signedJWT = SignedJWT.parse(token);
+            return new Jwt(
+                    token,
+                    Instant.ofEpochMilli(signedJWT.getJWTClaimsSet().getIssueTime().getTime()),
+                    Instant.ofEpochMilli(signedJWT.getJWTClaimsSet().getExpirationTime().getTime()),
+                    signedJWT.getHeader().toJSONObject(),
+                    signedJWT.getJWTClaimsSet().toJSONObject()
+            );
+            } catch (ParseException e) {
+            throw new JwtException("Invalid Token");
         }
 
-        if (Objects.isNull(nimbusJwtDecoder)) {
-            SecretKeySpec secretKeySpec = new SecretKeySpec(signerKey.getBytes(), "HS512");
-            nimbusJwtDecoder = NimbusJwtDecoder.withSecretKey(secretKeySpec)
-                    .macAlgorithm(MacAlgorithm.HS512)
-                    .build();
-        }
-
-        return nimbusJwtDecoder.decode(token);
     }
 }
